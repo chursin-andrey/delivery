d532729fbd24b64b85d1fbaac9d9a8ce75c3ce72
CHURSIN Andrey G <andrey.chursin@raiffeisen.ru>
2025-04-09 03:02:00 +0300

    module-6: add repository
    - Implemented Order and Courier repositories.

--- src/main/resources/db.migration/V1__init_db.sql
+++ src/main/resources/db.migration/V1__init_db.sql
@@ -1,7 +1,1 @@
-CREATE USER delivery WITH PASSWORD 'delivery';
-CREATE DATABASE delivery WITH OWNER delivery;
-
-GRANT ALL ON DATABASE delivery TO delivery;
-GRANT ALL ON SCHEMA public TO delivery;
-
 CREATE SCHEMA IF NOT EXISTS delivery;
--- src/test/java/ru/microarch/delivery/config/PostgresTestContainersConfiguration.java
+++ src/test/java/ru/microarch/delivery/config/PostgresTestContainersConfiguration.java
@@ -1,17 +1,55 @@
 package ru.microarch.delivery.config;
 
+import java.nio.file.Paths;
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
 @TestConfiguration
 public class PostgresTestContainersConfiguration {
 
+    private static final String POSTGRES_LATEST_VERSION = "postgres:latest";
+    private static final String SUPERUSER_LOGIN = "postgres";
+    private static final String SUPERUSER_PASSWORD = "postgres";
+    private static final String DATABASE_NAME = "delivery";
+    private static final String APPLICATION_CREDENTIALS = "delivery";
+    private static final Integer POSTGRES_PORT = 5432;
+
     @Bean
     @ServiceConnection
     public PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        final var postgresContainer = PostgreSQLContainer<>(DockerImageName.parse(POSTGRES_LATEST_VERSION))
+                .withUsername(SUPERUSER_LOGIN)
+                .withPassword(SUPERUSER_PASSWORD)
+                .withDatabaseName(DATABASE_NAME)
+                .withExposedPorts(POSTGRES_PORT)
+                .withCopyFileToContainer(
+                        MountableFile.forHostPath(
+                                Paths.get(".", "/docker/init/init.sql")
+                                        .normalize()
+                                        .toAbsolutePath()
+                        ),
+                        "/docker-entrypoint-initdb.d/"
+                );
+
+        postgresContainer.start();
+        return postgresContainer;;
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSource(PostgreSQLContainer postgreSqlContainer) {
+        return DataSourceBuilder.create()
+                .driverClassName("org.postgresql.Driver")
+                .url("jdbc:postgresql://localhost:" + postgreSqlContainer.getMappedPort(POSTGRES_PORT) + "/" + DATABASE_NAME)
+                .username(APPLICATION_CREDENTIALS)
+                .password(APPLICATION_CREDENTIALS)
+                .build();
     }
 }
